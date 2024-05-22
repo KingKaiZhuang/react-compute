4390c9d643adb1c92030ec92f1e027cbefff2bdf
Zhuang Jun Kai <113646306+KingKaiZhuang@users.noreply.github.com>
2024-05-22 15:24:20 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import MyCalculator from "./components/MyCalculator";
+
+function App() {
+  return (
+    <>
+      <h1>MyCalculator 5b0g0024 - 鈞凱</h1>
+      <MyCalculator />
+    </>
+  );
+}
+
+export default App;
